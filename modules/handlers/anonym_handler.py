ed6aea05cb8b186d927d52e4ad90743c22794289
Guberlo <salasero24@gmail.com>
2022-12-19 17:17:31 +0100

    feat: remove command from message

--- modules/handlers/anonym_handler.py
+++ modules/handlers/anonym_handler.py
@@ -1,14 +1,14 @@
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
 
-GROUP_ID = -1001863199799
+GROUP_ID = -1631780791
 
 def send_anonym_message(update: Update, context: CallbackContext):
-    """Handles the /send_anonym command.
+    """Handles the /send_anonymous command.
     Sends an anonymous message to the group
     Args:
         update: update event
         context: context passed by the handler
     """
 
-    context.bot.send_message(chat_id=GROUP_ID, text=update.message.text)
+    context.bot.send_message(chat_id=GROUP_ID, text=update.message.text.replace('/send_anonym', ''))
