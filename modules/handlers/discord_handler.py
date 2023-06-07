27691fe7191d74412607067ae1820b7159835082
guberlo <salvo.asero@neodatagroup.com>
2023-06-07 15:43:18 +0200

    feat: add discord user info on list

--- modules/handlers/discord_handler.py
+++ modules/handlers/discord_handler.py
@@ -7,6 +7,7 @@ import requests
 import os
 import random
 import ast
+import json
 
 config = Config()
 
@@ -19,15 +20,28 @@ def count_files(directory):
 def get_voice_members(update: Update, context: CallbackContext) -> str:
     users = requests.get(f'http://{config.discord_host}:{config.discord_port}')
     if users.text == '[]':
-        update.message.reply_animation(getRandomEmptyGif())
+        update.message.reply_animation(get_random_empty_gif())
     else:
-        users = ast.literal_eval(users.text)
+        users = json.loads(users.text)
         update.message.reply_text(
-            text='\n'.join(users)
+            text='\n'.join(map(lambda x: add_emoji_to_name(x), users))
         )
 
-def getRandomEmptyGif() -> str:
+def get_random_empty_gif() -> str:
     n_files = count_files(config.discord_gif_path)
     random_gif = random.randint(1, n_files)
     return open(f'{config.discord_gif_path}dead-server-{random_gif}.gif', 'rb').read()
+
+def add_emoji_to_name(user_info) -> str:
+    suffix = "      "
+    if user_info['muted']:
+        suffix += "🤐"
+    if user_info['deaf']:
+        suffix += "🔇"
+    if user_info['cam']:
+        suffix =+ "📸"
+    if user_info['streaming']:
+        suffix =+ "🔴"
+    
+    return user_info['name'] + suffix
 
