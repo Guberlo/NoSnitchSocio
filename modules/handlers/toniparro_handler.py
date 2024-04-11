54f85e42e55c8f4245f8421a27332696a91673c5
guberlo <salvo.asero@neodatagroup.com>
2024-04-11 11:48:08 +0200

    feat: add method to scrape linkvertise

--- modules/handlers/toniparro_handler.py
+++ modules/handlers/toniparro_handler.py
@@ -11,6 +11,24 @@ from modules.utils.message_paginator import split_messages
 
 import requests
 import re
+import time
+import base64
+import json
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+
+# extension_path = "/home/guberlo/NoSnitchSocio/config/ublock_origin-1.51.0.xpi"
+# service = Service()
+# options = Options()
+# # options.add_argument('--headless')
+# # options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0")
+# driver = webdriver.Firefox(service=service, options=options)
+# driver.install_addon(extension_path, temporary=True)
 
 config = Config()
 mysql = MysqlConnection(config)
@@ -91,16 +109,64 @@ def handle_names_pagination_callback(update: Update, context: CallbackContext):
         reply_markup=paginator.markup
     )
 
+def scrape_linkvertise(url: str) -> str: 
+    first_link = 'https://publisher.linkvertise.com/api/v1/redirect/link/static/'
+    second_link = 'https://publisher.linkvertise.com/api/v1/redirect/link/insert/linkvertise/path/here/target?serial=base64encodedjson'
+    second_link_front = second_link[0:second_link.find('insert/linkvertise')]
+    second_link_back = second_link[second_link.find('/target?serial'):second_link.find('base64encodedjson')]
+
+    new_link = "None"
+
+    try:
+        input_link = url
+        link = ''
+        if '.com/' in input_link:
+            if '?o=' in input_link:
+                link = input_link[input_link.find('.com/')+5:input_link.find('?o=')]
+            else:
+                link = input_link[input_link.find('.com/')+5:len(input_link)]
+        if '.net/' in input_link:
+            if '?o=' in input_link:
+                link = input_link[input_link.find('.net/')+5:input_link.find('?o=')]
+            else:
+                link = input_link[input_link.find('.net/')+5:len(input_link)]
+            
+
+        r = requests.get(first_link + link,timeout=2)
+        text = r.text
+        link_id = text[text.find('"id":')+5:text.find(',"url":')]
+
+        new_json = {"timestamp":int(time.time()), "random":"6548307", "link_id":int(link_id)}
+
+        s = json.dumps(new_json)
+        json_converted = base64.b64encode(s.encode('utf-8'))
+        json_converted = str(json_converted)
+        json_converted = json_converted[2:len(json_converted)-1]
+
+        r = requests.get(second_link_front + link + second_link_back + json_converted,timeout=4)
+        converted_json = json.loads(r.text)
+        new_link = converted_json['data']['target']
+
+    except:
+        filler_value = "filler_value"
+    
+    return new_link
+
 def scrape_link(url: str) -> Dict[str, str]:
-    params['url'] = url
-    html = requests.get(scrape_ops_url, params=params)
-    soup = BeautifulSoup(html.text, "html.parser")
-    page_script = soup.select("body > script:nth-child(4)")[0].text
+    if ("link-hub" in url):
+        scraped_link = scrape_linkvertise(url)
+        name = "Unknown"
+    else:
+        params['url'] = url
+        html = requests.get(scrape_ops_url, params=params)
+        soup = BeautifulSoup(html.text, "html.parser")
+        page_script = soup.select("body > script:nth-child(4)")[0].text
+        name =  re.search("title: '(.*?)\',", page_script).group(1)
 
     return {
-        'name': re.search("title: '(.*?)\',", page_script).group(1),
+        'name': name,
         'link': url,
-        'scraped_link': re.search("link: '(.*?)\',", page_script).group(1)
+        'scraped_link': scraped_link
     }
 
 def save_scraped_link(scraped: Dict[str, str]) -> bool:
