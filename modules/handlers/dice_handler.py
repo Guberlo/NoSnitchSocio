d1b539500a74bb06aafd1564629721a8c49d3d91
Salvo Asero <s.asero@onetag.com>
2024-11-18 12:03:28 +0100

    feat: prometheus metrics WIP

--- modules/handlers/dice_handler.py
+++ modules/handlers/dice_handler.py
@@ -6,10 +6,14 @@ from telegram.ext import CallbackContext
 from modules.data.config import Config
 from modules.data.database import MysqlConnection
 from modules.handlers.save_action_handler import send_post_to_admins
+from prometheus_client import Counter
 
 
 config = Config()
 mysql = MysqlConnection(config)
+
+dice_counter = Counter('dice_counter', 'Counts the number of dice commands issued')
+dice_queries_counter = Counter('dice_queries_counter', 'Counts the number of queries issued within the dice command')
 
 prefix = "Se esce"
 COMMAND = "!dado"
@@ -17,13 +21,16 @@ COMMAND = "!dado"
 def get_random_action() -> str:
     query = f"SELECT id FROM {config.db_database}.actions"
     result = mysql.select(query)
+    dice_queries_counter.inc()
     ids = [item for t in result for item in t]
     
     query = f"SELECT description FROM {config.db_database}.actions a WHERE a.id={random.choice(ids)}"
     description = mysql.select(query)
+    dice_queries_counter.inc()
     return description[0][0]
 
 def handle_dice(update: Update, context: CallbackContext):
+    dice_counter.inc()
     if update.message.text == COMMAND or update.message.text == COMMAND + " random":
         random_action = get_random_action()
         update.message.reply_text(f"{prefix} {random.randint(1,6)} {random_action}")
